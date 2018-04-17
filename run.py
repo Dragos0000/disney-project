aa795724c19f0318c08895cb0b6ccf90a1a3e8ba
Dragos <dragos.paun0000@gmail.com>
2018-04-17 10:43:24 +0100

    added home page, footer and posts database

--- run.py
+++ run.py
@@ -8,8 +8,33 @@ app.secret_key = "some secret"
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	names = []
+	posts = []
+	with open("data/post.txt", "r") as file:
+		lines = file.read().splitlines()
 
+	for i, text in enumerate(lines):
+		if i%2 == 0:
+			names.append(text)
+		else:
+			posts.append(text)
+	names_and_posts = zip(names, posts)
+
+	
+	return render_template("index.html", data= names_and_posts)
+
+@app.route('/',methods=["POST"])
+def form_blog():
+	
+	text = request.form["message"]
+	user_name = request.form['name']
+	file = open("data/post.txt", "a")
+	file.write(user_name + "\n")
+	file.write(text + "\n")
+	file.close()
+	if request.method == "POST":
+		flash("Thanks {}, we have received your princess quote! Write one more or revisit Home page to see your post".format(request.form["name"]))
+	return render_template("index.html")
 
 @app.route('/about')
 def about():
